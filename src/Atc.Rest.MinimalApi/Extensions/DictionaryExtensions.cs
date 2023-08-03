b2678b33ecaa222822db82dfea0e763f76bff6de
Per Kops <pko@delegate.dk>
2023-08-03 18:11:52 +0200

    docs: Adding documentation to DictionaryExtensions

--- src/Atc.Rest.MinimalApi/Extensions/DictionaryExtensions.cs
+++ src/Atc.Rest.MinimalApi/Extensions/DictionaryExtensions.cs
@@ -2,6 +2,13 @@ namespace Atc.Rest.MinimalApi.Extensions;
 
 internal static class DictionaryExtensions
 {
+    /// <summary>
+    /// Merges two dictionaries containing errors and ensures there are no duplicate values for the same key.
+    /// </summary>
+    /// <param name="errorsA">The first dictionary of errors to merge.</param>
+    /// <param name="errorsB">The second dictionary of errors to merge.</param>
+    /// <returns>A new dictionary which includes the keys and values from both input dictionaries.
+    /// If the same key exists in both dictionaries, the values are merged and duplicates are removed.</returns>
     internal static IDictionary<string, string[]> MergeErrors(
         this IDictionary<string, string[]> errorsA,
         IDictionary<string, string[]> errorsB)
