d6c3dbdba0e3d38a1436388a71c2641da082e408
David Kallesen <david@lmdk.dk>
2024-04-24 23:29:53 +0200

    feat: Add WebApplicationExtensions with AddGlobalErrorHandler with optional use of GlobalErrorHandlingOptions

---
+++ src/Atc.Rest.MinimalApi/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,24 @@
+namespace Atc.Rest.MinimalApi.Extensions;
+
+/// <summary>
+/// Provides extension methods for the <see cref="WebApplication"/> class to enhance its functionality.
+/// This class specifically includes methods for adding global error handling capabilities to the application.
+/// </summary>
+public static class WebApplicationExtensions
+{
+    /// <summary>
+    /// Adds a global error handler middleware to the specified web application.
+    /// This middleware is designed to catch and process unhandled exceptions globally.
+    /// </summary>
+    /// <param name="app">The <see cref="WebApplication"/> to which the error handling middleware is added.</param>
+    /// <param name="configureOptions">An optional action to configure the <see cref="GlobalErrorHandlingOptions"/>.</param>
+    /// <returns>The <see cref="IApplicationBuilder"/> with the error handling middleware configured.</returns>
+    public static IApplicationBuilder AddGlobalErrorHandler(
+        this WebApplication app,
+        Action<GlobalErrorHandlingOptions>? configureOptions = null)
+    {
+        var options = new GlobalErrorHandlingOptions();
+        configureOptions?.Invoke(options);
+        return app.UseMiddleware<GlobalErrorHandlingMiddleware>(options);
+    }
+}
