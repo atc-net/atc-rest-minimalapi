af65a47d5f27c54765a43b3063418fa7792dad9f
Per Kops <pko@delegate.dk>
2025-12-03 12:05:00 +0100

    fix(sample): improve validation and fix nullable handling in request models

--- sample/src/Demo.Domain/Validators/CreateUserRequestValidator.cs
+++ sample/src/Demo.Domain/Validators/CreateUserRequestValidator.cs
@@ -7,25 +7,28 @@ public sealed class CreateUserRequestValidator : AbstractValidator<CreateUserReq
         RuleFor(x => x.FirstName)
             .NotNull()
             .Length(2, 10)
-            .Matches(@"^[A-Z]")
+            .Matches("^[A-Z]")
             .WithMessage(x => $"{nameof(x.FirstName)} has to start with an uppercase letter.");
 
         RuleFor(x => x.LastName)
             .NotNull()
             .Length(2, 30)
-            .Matches(@"^[A-Z]")
+            .Matches("^[A-Z]")
             .WithMessage(x => $"{nameof(x.LastName)} has to start with an uppercase letter.")
             .NotEqual(x => x.FirstName)
             .WithMessage("FirstName must not be equal to LastName.");
 
         RuleFor(x => x.WorkAddress).NotNull();
 
-        RuleFor(x => x.WorkAddress.CityName)
-            .NotEmpty()
-            .WithMessage("WorkAddress.CityName is required.")
-            .MinimumLength(3)
-            .WithMessage("WorkAddress.CityName must be min 3 characters long.")
-            .MaximumLength(20)
-            .WithMessage("WorkAddress.CityName must be max 20 characters long.");
+        When(x => x.WorkAddress is not null, () =>
+        {
+            RuleFor(x => x.WorkAddress.CityName)
+                .NotEmpty()
+                .WithMessage("WorkAddress.CityName is required.")
+                .MinimumLength(3)
+                .WithMessage("WorkAddress.CityName must be min 3 characters long.")
+                .MaximumLength(20)
+                .WithMessage("WorkAddress.CityName must be max 20 characters long.");
+        });
     }
 }
--- sample/src/Demo.Domain/Validators/UpdateUserRequestValidator.cs
+++ sample/src/Demo.Domain/Validators/UpdateUserRequestValidator.cs
@@ -28,10 +28,16 @@ public sealed partial class UpdateUserRequestValidator : AbstractValidator<Updat
         RuleFor(x => x.Request.WorkAddress)
             .NotNull();
 
-        RuleFor(x => x.Request.WorkAddress.CityName)
-            .NotNull()
-            .Length(2, 10)
-            .WithMessage("CityName is too short.");
+        When(x => x.Request.WorkAddress is not null, () =>
+        {
+            RuleFor(x => x.Request.WorkAddress!.CityName)
+                .NotEmpty()
+                .WithMessage("WorkAddress.CityName is required.")
+                .MinimumLength(3)
+                .WithMessage("WorkAddress.CityName must be min 3 characters long.")
+                .MaximumLength(50)
+                .WithMessage("WorkAddress.CityName must be max 50 characters long.");
+        });
     }
 
     [GeneratedRegex("^[A-Z]", RegexOptions.ExplicitCapture, matchTimeoutMilliseconds: 1000)]
