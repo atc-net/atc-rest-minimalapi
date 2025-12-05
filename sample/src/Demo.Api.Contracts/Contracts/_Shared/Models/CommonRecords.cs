7be6e3df943d55850c4b106dbd3efe914b698a1b
Per Kops <pko@delegate.dk>
2025-12-05 20:23:06 +0100

    fix(sample): make Address and Country record fields nullable

--- sample/src/Demo.Api.Contracts/Contracts/_Shared/Models/CommonRecords.cs
+++ sample/src/Demo.Api.Contracts/Contracts/_Shared/Models/CommonRecords.cs
@@ -2,13 +2,13 @@
 namespace Demo.Api.Contracts.Contracts;
 
 public sealed record Address(
-    [property: StringLength(255)] string StreetName,
-    string StreetNumber,
-    string PostalCode,
-    [property: JsonPropertyName("cityName")] string CityName,
-    Country Country);
+    [property: StringLength(255)] string? StreetName,
+    string? StreetNumber,
+    string? PostalCode,
+    [property: JsonPropertyName("cityName")] string? CityName,
+    Country? Country);
 
 public record Country(
-    string Name,
-    [property: MinLength(2), MaxLength(2), RegularExpression("^[A-Za-z]{2}$")] string Alpha2Code,
-    [property: MinLength(3), MaxLength(3), RegularExpression("^[A-Za-z]{3}$")] string Alpha3Code);
+    string? Name,
+    [property: MinLength(2), MaxLength(2), RegularExpression("^[A-Za-z]{2}$")] string? Alpha2Code,
+    [property: MinLength(3), MaxLength(3), RegularExpression("^[A-Za-z]{3}$")] string? Alpha3Code);
